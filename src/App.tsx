52b799bab26e55ed9de02b81cb070ecf413e805e
Daltimore <obinnaleo78@gmail.com>
2023-12-05 12:51:32 +0100

    Project bootstrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { AppRoutes } from './enums/Routes'
 import './App.css';
 
-function App() {
+// Pages
+import DashboardLanding from './pages/index'
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <Router>
+        <Routes>
+          <Route path={AppRoutes.landing} element={<DashboardLanding />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
